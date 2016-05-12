74832ed734af69aeab05b05852f004bdb40d9d1e
aoisupersix <aoi_supersix@yahoo.co.jp>
2016-05-12 18:19:34 +0900

    [add]結果画面のアニメーションを追加

--- app/src/main/java/supersix/aoi/vacancy/ImageArrayAdapter.java
+++ app/src/main/java/supersix/aoi/vacancy/ImageArrayAdapter.java
@@ -4,23 +4,25 @@ import java.util.List;
 import supersix.aoi.vacancy.R;
 import supersix.aoi.vacancy.Listitem;
 import android.content.Context;
+import android.graphics.Color;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class ImageArrayAdapter extends ArrayAdapter<Listitem> {
 
-    private int resourceId;
     private List<Listitem> items;
     private LayoutInflater inflater;
 
     public ImageArrayAdapter(Context context, int resourceId, List<Listitem> items) {
         super(context, resourceId, items);
 
-        this.resourceId = resourceId;
         this.items = items;
         this.inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
@@ -31,7 +33,8 @@ public class ImageArrayAdapter extends ArrayAdapter<Listitem> {
         if (convertView != null) {
             view = convertView;
         } else {
-            view = this.inflater.inflate(this.resourceId, null);
+            inflater = (LayoutInflater)getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = this.inflater.inflate(R.layout.item_layout, null);
         }
 
         Listitem item = this.items.get(position);
@@ -43,6 +46,11 @@ public class ImageArrayAdapter extends ArrayAdapter<Listitem> {
         // アイコンをセット
         ImageView appInfoImage = (ImageView)view.findViewById(R.id.listrow_ImageView);
         appInfoImage.setImageResource(item.getImageId());
+
+        // XMLで定義したアニメーションを読み込む
+        Animation anim = AnimationUtils.loadAnimation(getContext(), R.anim.item_motion);
+        // リストアイテムのアニメーションを開始
+        view.startAnimation(anim);
 
         return view;
     }
